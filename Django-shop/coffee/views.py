aabaaa4972f8db3a9639885889921159258a3bce
kageyamayuuto <andiihp69@gmail.com>
2024-08-16 07:04:09 +0700

    first commit

---
+++ Django-shop/coffee/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Coffee
+
+# Create your views here.
+def home(request):
+    coffee = Coffee.objects.all()
+    # return HttpResponse('<h1>hello world</h1>')
+    return render(request, 'home.html', {'coffee' : coffee})
+    
+    
